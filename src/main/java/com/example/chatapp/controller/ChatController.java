91a03a1cba39cd2102d55db8a5611a20ddefd2ee
임빈영 <qlsdud960604@gmail.com>
2021-09-25 12:54:11 +0900

    Controller 영역 구현

---
+++ src/main/java/com/example/chatapp/controller/ChatController.java
@@ -0,0 +1,32 @@
+package com.example.chatapp.controller;
+
+import com.example.chatapp.domain.Chat;
+import com.example.chatapp.repository.ChatRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.*;
+import reactor.core.publisher.Flux;
+import reactor.core.publisher.Mono;
+import reactor.core.scheduler.Schedulers;
+
+import java.time.LocalDateTime;
+
+@RequiredArgsConstructor
+@RestController   // 데이터를 리턴하는 서버
+public class ChatController {
+
+    private final ChatRepository chatRepository;
+
+    @GetMapping(value = "/sender/{sender}/receiver/{receiver}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<Chat> getMessage(@PathVariable String sender, @PathVariable String receiver) {
+        return chatRepository.mFindBySender(sender, receiver)
+                .subscribeOn(Schedulers.boundedElastic());
+    }
+
+    @PostMapping("/chat")
+    public Mono<Chat> setMessage(@RequestBody Chat chat) {
+        chat.setCreatedAt(LocalDateTime.now());
+
+        return chatRepository.save(chat);
+    }
+}
---
+++ src/main/java/com/example/chatapp/repository/ChatRepository.java
@@ -0,0 +1,14 @@
+package com.example.chatapp.repository;
+
+import com.example.chatapp.domain.Chat;
+import org.springframework.data.mongodb.repository.Query;
+import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import org.springframework.data.mongodb.repository.Tailable;
+import reactor.core.publisher.Flux;
+
+public interface ChatRepository extends ReactiveMongoRepository<Chat, String> {
+
+    @Tailable   // 커서를 안닫고 계속 유지
+    @Query("{sender:?0, receiver:?1}")
+    Flux<Chat> mFindBySender(String sender, String receiver);  // Flux : response를 유지하면서 데이터를 계속 흘려보낼 수 있음
+}
